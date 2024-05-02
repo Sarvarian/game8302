260fdb8f34c99551dbdc33eb89f8dabe7272d1d1
Sarvarian <Sarvarian@Hotmail.com>
2024-05-02 16:03:16 +0330

    update build scripts again again

--- build.ps1
+++ build.ps1
@@ -1,5 +1,1 @@
-
-$compilation_files = Get-Content -Path .\compilation_files.txt # -TotalCount 1
-$compilation_files = $compilation_files.Trim()
-
-cl /EHsc $compilation_files /link SDL2.lib SDL2main.lib shell32.lib /SUBSYSTEM:CONSOLE
+cl /EHsc main.cpp /link SDL2.lib SDL2main.lib shell32.lib /SUBSYSTEM:CONSOLE
--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-compilation_files=$(xargs < compilation_files.txt)
-
-clang++ -W -mconsole $compilation_files -L/./ -lmingw32 -lsdl2main -lsdl2 -o game.exe
+clang++ -W -mconsole main.cpp -L/./ -lmingw32 -lsdl2main -lsdl2 -o game.exe
