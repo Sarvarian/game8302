98cd36f5a1b4369a346251f3ec5ca819e00eac88
Sarvarian <Sarvarian@Hotmail.com>
2024-05-04 13:53:23 +0330

    Update helper.py

--- helper.py
+++ helper.py
@@ -4,7 +4,7 @@ import os
 import os.path
 
 
-def read_content(file_path):
+def read_content(file_path: str) -> str:
     """Returns content of a text file.
     file_path: Give a full path. (relative or absolute does not matter.)
     """
@@ -14,7 +14,7 @@ def read_content(file_path):
         return content
 
 
-def write_content(file_path, content):
+def write_content(file_path: str, content: str) -> None:
     """Write content to a text file.
     file_path: Give a full path. (relative or absolute does not matter.)
     content: String of text.
@@ -24,12 +24,12 @@ def write_content(file_path, content):
         file.close()
 
 
-def read_types(file_path):
+def read_types(file_path: str) -> list[dict[str, str]]:
     """Returns a list of dictionaries of types.
     file_path: Give full path to types list file. (relative or absolute does not matter.)
     """
     types = read_content(file_path).split()
-    res = []
+    res: list[dict[str, str]] = []
     i = 0
     while i < len(types):
         res.append(
@@ -38,12 +38,12 @@ def read_types(file_path):
     return res
 
 
-def read_routine_templates(dir_path):
+def read_routine_templates(dir_path: str) -> dict[str, str]:
     """Returns a dictionary of types and their routines.
     dir_path: Give full path to routine template directory. (relative or absolute does not matter.)
     """
     names = os.listdir(dir_path)
-    res = {}
+    res: dict[str, str] = {}
     for name in names:
         path = os.path.join(dir_path, name)
         if os.path.isdir(path):
