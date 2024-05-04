ac7890a1c97167393bc7df7f1bfa696b8ce323ee
Sarvarian <Sarvarian@Hotmail.com>
2024-05-04 10:30:43 +0330

    update

--- helper.py
+++ helper.py
@@ -11,14 +11,25 @@ def read_content(file_path):
         return content
 
 
+def write_content(file_path, content):
+    """Write content to a text file.
+    file_path: Give full path to types list file.
+    content: String of text.
+    """
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+        file.close()
+
+
 def read_types(file_path):
-    """ Returns a list of dictionaries of types.
+    """Returns a list of dictionaries of types.
     file_path: Give full path to types list file.
     """
     types = read_content(file_path).split()
     res = []
     i = 0
     while i < len(types):
-        res.append({'raw': types[i], 'type': types[i+1]})
-        i += 2
+        res.append(
+            {'raw': types[i], 'type': types[i+1], 'default': types[i+2]})
+        i += 3
     return res
