938213ab0d00773d8426b18764cc8f1e8e005c02
Sarvarian <Sarvarian@Hotmail.com>
2024-05-04 13:30:08 +0330

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -13,7 +13,15 @@ int main(int argc, char** argv)
 	cmdline_argc = argc;
 	cmdline_argv = argv;
 
-	auto x = 0.0;
+	f32 x = 0.0f;
+
+	x = 3;
+
+	f32 y = x;
+
+	y = x.mul(y);
+
+	printf("Number is: %f", y.raw());
 
 	return 0;
 }
