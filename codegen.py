7988b3686858b09430549925624d4f35db627100
Sarvarian <Sarvarian@Hotmail.com>
2024-05-06 14:18:48 +0330

    Update codegen.py

--- codegen.py
+++ codegen.py
@@ -64,6 +64,10 @@ floor arg
 trunc arg
 """.strip()
 
+C_MATH_FUNCTION_TEMPLATE = """
+inline $type_name cpp_std_$func_name$suffix($typed_args) { return $func_name$suffix($input); }$new_line
+""".strip()
+
 
 def generate_c_math_functions_director() -> str:
     """ Generate a list of functions that points'
@@ -77,8 +81,7 @@ def generate_c_math_functions_director() -> str:
     ]
     functions = list_of_c_math_functions.splitlines()
     functions = map(lambda x: x.strip().split(), functions)
-    temp = 'inline $type_name cpp_std_$func_name$suffix($typed_args) { return $func_name$suffix($input); }\n'
-    temp = Template(temp)
+    temp = Template(C_MATH_FUNCTION_TEMPLATE)
 
     res = ''
     for func in functions:
@@ -96,6 +99,7 @@ def generate_c_math_functions_director() -> str:
             values['func_name'] = func[0]
             values['typed_args'] = typed_args
             values['input'] = typed_args.replace(f'{type_name} ', '')
+            values['new_line'] = '\n'
             res += temp.substitute(values)
     res = res.strip()
     return f"""
