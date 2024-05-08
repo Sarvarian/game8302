402d7b9eecc4df690f3992e249d2efdd2f9464ac
Sarvarian <Sarvarian@Hotmail.com>
2024-05-08 21:04:26 +0330

    update

--- code_generation/constructor.py
+++ code_generation/constructor.py
@@ -2,34 +2,27 @@
 """
 
 
-from code_generation.method import generate_arguments, generate_method
+from code_generation.method import generate_method
 from code_generation.types import MethodGenerationData
 
 
 def generate_constructor_methods_for_type(data: MethodGenerationData) -> str:
     """ Generate constructor methods for the given type.
     """
+    members = data.member_names_without_suffix_underscore
     data.method_name = data.type_name
     data.method_return_type = ''
-    members = data.member_names_without_suffix_underscore
-    arguments: list[list[str]] = []
-    arg_inits = ''
-    default_inits = ''
-    for member in members:
-        arguments.append([data.base_type_name, member])
-        arg_inits += f'{member}_({member}), '
-        default_inits += f'{member}_({data.default_value}), '
-    arg_inits = arg_inits.removesuffix(', ')
-    default_inits = default_inits.removesuffix(', ')
-    data.method_arguments = generate_arguments(arguments)
-    data.const = f' : {arg_inits}'
-    elements = \
-        list(map(lambda x: x.strip(), generate_method(data).splitlines()))
-    init_constructor = f'\t{elements[0]} {elements[1]}{elements[3]}\n\n'
-    data.const = f' : {default_inits}'
+    data.const = f' : {members[0]}_({members[0]}) '
+    data.method_arguments = f'{data.base_type_name} {', '.join(members)}'
+    init_constructor = \
+        generate_method(data) \
+        .replace('\t ', '\t') \
+        .replace('\n\t{\n\t\t\n\t}', '{}')
+    s = f'_({data.default_value}), '
+    data.const = f' : {s.join(members)}{s.removesuffix(', ')} '
     data.method_arguments = ''
-    elements = \
-        list(map(lambda x: x.strip(), generate_method(data).splitlines()))
-    default_constructor = f'\t{
-        elements[0]} {elements[1]}{elements[3]}\n\n'
+    default_constructor = \
+        generate_method(data) \
+        .replace('\t ', '\t') \
+        .replace('\n\t{\n\t\t\n\t}', '{}')
     return init_constructor + default_constructor
