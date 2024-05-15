4caa84daecb0d31dfd24a8f3fa5fe3445d626cf7
Sarvarian <Sarvarian@Hotmail.com>
2024-05-15 04:34:46 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-clang++ -W -std=c++98 -mconsole main2.cpp -L/./ -lmingw32 -lsdl2main -lsdl2 -o game.exe
+clang++ -std=c++98 -Wall -pedantic -mconsole main2.cpp -L/./ -lmingw32 -lsdl2main -lsdl2 -o game.exe
