496252995ea0933af923d17d8877d0dbc274b4ee
Sarvarian <Sarvarian@Hotmail.com>
2024-05-15 05:05:57 +0330

    Update pch.hpp

--- pch.hpp
+++ pch.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <vector>
-#include <iostream>
-#include <cmath>
+#include <stdio.h>
+#include <math.h>
+#undef SIZE_MAX
+#define SIZE_MAX ((size_t) -1)
 #include "SDL2/SDL.h"
